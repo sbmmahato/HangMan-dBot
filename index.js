e6ff7ca97e4f2764ef1e2ed4b7016fe1035bb8b6
Subham Mahato <sbmmahato@gmail.com>
2024-04-10 21:15:40 +0530

    fixed bugs and added more feature

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 const list=['yo','word','elephant'];
-const b=list.length+1;
+const b=list.length;
 var a=null;
 
 var repeat = function(str, count) {
@@ -15,6 +15,20 @@ var repeat = function(str, count) {
         array[i++] = str;
     return array.join('');
 }
+
+// async function generateWord() {
+//     return new Promise((resolve, reject) => {
+//         let a = list[Math.floor(Math.random() * b)];
+//         resolve(a);
+//     });
+// }
+// async function countWord(word){
+//     return new Promise((resolve,reject)=>{
+//         let c=word.length;
+//         console.log(c);
+//         resolve(c);
+//     })
+// }
 
 client.on('messageCreate',async (message)=>{
     if(message.author.bot) return;
@@ -27,9 +41,12 @@ client.on('interactionCreate',async (interaction)=>{
     //  n=[];
      if(interaction.isCommand()){
         if(interaction.commandName==='start'){
-            n=[]; count=7;
+            n=[]; count=8;
            a=list[Math.floor(Math.random()*b)];
-         await interaction.reply(`Guess the word: \n\`${repeat('_ ',a.length)}\``)
+            // let a = await generateWord();
+            let c=a.length ;
+            // let c = await countWord(a);
+         await interaction.reply(`Guess the word: \n\`${repeat('_ ',c)}\``)
         for(let i=0;i<a.length;i++){
             n.push('_');
         }
@@ -40,6 +57,7 @@ client.on('interactionCreate',async (interaction)=>{
 client.on('interactionCreate',async (interact)=>{
     if( interact.commandName==='g'){
         if(a){
+            let length=a.length;
             const text=await interact.options.getString("letter");
             var z=0;
             for(let i=0;i<a.length;i++){
@@ -48,11 +66,11 @@ client.on('interactionCreate',async (interact)=>{
                 }
             }
             // interact.reply(`\`${n.join(' ')}\``);
-            if(z===1){await interact.reply(`\`${n.join(' ')}\``)}else{
-                console.log(z);
+            if(z===1 && count!==0){await interact.reply(`\`${n.join(' ')}\``)}else if(z===0 && count!==0){
                 count--;
                 await interact.reply(`'${text}' does not exist in the word.\nYou have ${count} chances left.\n\`${n.join(' ')}\``)
             }
+            if(count===0){await interact.reply(`You used up all your chances. Better Luck next time!`)}
         }else{
             await interact.reply('do /start first');
         }
